688855392456be3e80e26cebcf0c8017df9364d3
soominok <sumin00331@naver.com>
2020-10-26 18:48:42 +0900

    18:48 commit

--- com_cheese_api/ext/routes.py
+++ com_cheese_api/ext/routes.py
@@ -3,6 +3,7 @@ from com_cheese_api.cheese.api import Cheese
 from com_cheese_api.fnq.api import Fnq
 from com_cheese_api.recommend.api import Recommend
 from com_cheese_api.admin.api import Admin
+from com_cheese_api.user.api import User
 
 def initialize_routes(api):
     api.add_resource(Home, '/api')
@@ -10,3 +11,4 @@ def initialize_routes(api):
     api.add_resource(Fnq, '/api/fnq')
     api.add_resource(Recommend, '/api/recommend')
     api.add_resource(Admin, '/api/admin')
+    api.add_resource(User, '/api/user')
