123f8a423e783cffec64803a1235dc08d89938d7
soominok <sumin00331@naver.com>
2020-11-04 02:07:23 +0900

    make object 1

---
+++ com_cheese_api/resources/practice/abouts.py
@@ -0,0 +1,8 @@
+import os
+import matplotlib
+#print("current working directory: " + os.getcwd())
+#print("absolute path : " + os.path.abspath("\data"))
+#print("find directory name: " + os.path.dirname(__file__))
+#print("new create directory path: " + os.path.join(os.path.abspath(os.path.dirname(__file__))+'/data'))
+
+print('설정위치: ', matplotlib.__file__)
--- com_cheese_api/resources/user.py
+++ com_cheese_api/resources/user.py
@@ -6,6 +6,8 @@ from konlpy.tag import Okt
 from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 
@@ -26,54 +28,59 @@ class UserDf():
         self.odf = None
 
     def category_Count(self):
-    users = self.data('users.csv')
-    sub_size = users['buy_count'].groupby(users['sub1_category']).sum().reset_index(name='sub1_counts')
-    sub_size['sub1_rank'] = sub_size['sub1_counts'].rank(ascending=False)
-    item_count_plot = sns.barplot (x = 'sub1_category', y = 'sub1_counts', data = sub_size)
-    # plt.show()
-    # print(sub_size)
-    return sub_size
+        users = self.data('users.csv')
+        sub_size = users['buy_count'].groupby(users['sub1_category']).sum().reset_index(name='sub1_counts')
+        sub_size['sub1_rank'] = sub_size['sub1_counts'].rank(ascending=False)
+        item_count_plot = sns.barplot (x = 'sub1_category', y = 'sub1_counts', data = sub_size)
+        # plt.show()
+        # print(sub_size)
+        return sub_size
+    #category_Count()
 
-#category_Count()
+    def item_Count():
+        # sub1_category에서의 빈도 추출
+        category_count = category_Count()
 
-def item_Count():
-    # sub1_category에서의 빈도 추출
-    category_count = category_Count()
+        # sub2_category에서의 빈도 추출
+        item_size = users['buy_count'].groupby([users['sub1_category'],users['sub2_category']]).sum().reset_index(name='sub2_counts')
+        
+        #item_size['sub2_rank'] = item_size['sub2_counts'].rank(ascending=False, method="dense")
+        item_size['sub2_rank'] = item_size['sub2_counts'].rank(ascending=False)
 
-    # sub2_category에서의 빈도 추출
-    item_size = users['buy_count'].groupby([users['sub1_category'],users['sub2_category']]).sum().reset_index(name='sub2_counts')
-    
-    #item_size['sub2_rank'] = item_size['sub2_counts'].rank(ascending=False, method="dense")
-    item_size['sub2_rank'] = item_size['sub2_counts'].rank(ascending=False)
+        # sub1 + sub2 rank 합치기
+        category_item_rank = pd.merge(category_count, item_size, on = 'sub1_category', how = 'right')
+        # print(category_item_rank)
 
-    # sub1 + sub2 rank 합치기
-    category_item_rank = pd.merge(category_count, item_size, on = 'sub1_category', how = 'right')
-    # print(category_item_rank)
+        # 원래 데이터에 rank 주기 (중복에 대한 계산도 같이 해주기)
+        user_item_rank = pd.merge(users, category_item_rank, on = 'sub2_category', how = 'left')
+        print(user_item_rank)
 
-    # 원래 데이터에 rank 주기 (중복에 대한 계산도 같이 해주기)
-    user_item_rank = pd.merge(users, category_item_rank, on = 'sub2_category', how = 'left')
-    print(user_item_rank)
+        user_items_ranks = user_item_rank.drop(['sub1_category_y'], axis=1)
+        users_item_data = user_items_ranks.rename(columns={'sub1_category_x': 'sub1_category'})
 
-    user_items_ranks = user_item_rank.drop(['sub1_category_y'], axis=1)
-    users_item_data = user_items_ranks.rename(columns={'sub1_category_x': 'sub1_category'})
+        # users_item_data.to_csv(os.path.join('com_cheese_api/resources/data', 'user_item_counts3.csv'), index=False)
+        return users_item_data
 
-    # users_item_data.to_csv(os.path.join('com_cheese_api/resources/data', 'user_item_counts3.csv'), index=False)
-    return users_item_data
+    # item_count()
 
-# item_count()
+    @staticmethod
+    def cheeseData():
+        cheese_data = pd.read_csv("com_cheese_api/resources/data/cheese_data.csv")
+        return cheese_data
 
+    @staticmethod
+    def item_Change():
+        cheese_data = cheeseData()
+        users_item_data = item_Count()
+        cheese_df = cheese_data.rename(columns={'ranking': 'sub2_rank'})
+        users_data = pd.merge(users_item_data, cheese_data, on = 'sub2_rank', how = 'left')
+        user_datas = users_data.drop(['category_x', 'sub1_category', 'sub2_category', 'item_brand', 'sub1_counts', 'sub1_rank', 'sub2_counts', 'sub2_rank', 'content', 'buy_price', 'img'], axis=1)
+        user_data = user_datas.drop(['Unnamed: 0', 'country', 'matching', 'matching.1', 'texture', 'types', 'category_y'], axis=1)
+        user_data_fin = user_data.rename(columns={'brand': 'cheese_brand', 'name': 'cheese_name', 'price' : 'cheese_one_price'})
+        print(user_data_fin)
 
-def item_Change():
-    users_item_data = item_Count()
-    cheese_data = cheeses.rename(columns={'ranking': 'sub2_rank'})
-    users_data = pd.merge(users_item_data, cheese_data, on = 'sub2_rank', how = 'left')
-    user_datas = users_data.drop(['category_x', 'sub1_category', 'sub2_category', 'item_brand', 'sub1_counts', 'sub1_rank', 'sub2_counts', 'sub2_rank', 'content', 'buy_price', 'img'], axis=1)
-    user_data = user_datas.drop(['Unnamed: 0', 'country', 'matching', 'matching.1', 'texture', 'types', 'category_y'], axis=1)
-    user_data_fin = user_data.rename(columns={'brand': 'cheese_brand', 'name': 'cheese_name', 'price' : 'cheese_one_price'})
-    print(user_data_fin)
-
-    user_data_fin.to_csv(os.path.join('com_cheese_api/resources/data', 'user_data.csv'), index=False)
-#item_Change()
+        user_data_fin.to_csv(os.path.join('com_cheese_api/resources/data', 'user_data.csv'), index=False)
+    #item_Change()
 
 
 def show_df():
